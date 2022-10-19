d2fcce8358b064bc8683abb3b081c597c9d7b771
Jarod Timms <68355498+TimmsJarod@users.noreply.github.com>
2022-10-19 17:27:55 -0500

    Add files via upload

---
+++ pwmled2_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/pwmled2.d
@@ -0,0 +1,54 @@
+# FIXED
+
+pwmled2.obj: ../pwmled2.c
+pwmled2.obj: C:/ti/simplelink_cc32xx_sdk_6_10_00_05/kernel/nortos/posix/unistd.h
+pwmled2.obj: C:/ti/simplelink_cc32xx_sdk_6_10_00_05/kernel/nortos/posix/sys/types.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+pwmled2.obj: F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+pwmled2.obj: C:/ti/simplelink_cc32xx_sdk_6_10_00_05/source/ti/drivers/PWM.h
+pwmled2.obj: syscfg/ti_drivers_config.h
+pwmled2.obj: C:/ti/simplelink_cc32xx_sdk_6_10_00_05/source/ti/devices/DeviceFamily.h
+
+../pwmled2.c:
+
+C:/ti/simplelink_cc32xx_sdk_6_10_00_05/kernel/nortos/posix/unistd.h:
+
+C:/ti/simplelink_cc32xx_sdk_6_10_00_05/kernel/nortos/posix/sys/types.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+F:/Programs/Texas\ Instruments/CCS/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/simplelink_cc32xx_sdk_6_10_00_05/source/ti/drivers/PWM.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_cc32xx_sdk_6_10_00_05/source/ti/devices/DeviceFamily.h:
+
